2a6e56d1937ced362c273fdef5324cabcf5c6271
king-ellie <elliebking1@gmail.com>
2024-12-15 20:11:32 -0500

    delete pee pee poopoo?

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
 
-#pee pee poopoo poo poo 
 @app.route('/api/data')
 def get_data():
     return jsonify({"message": "Hello from our new new Flask backend!"})
